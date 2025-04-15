8147b4e2db567e425ee3048c9dd20a780c4afb7a
sstaar91 <sstaar91@gmail.com>
2025-04-15 09:44:28 +0900

    chore : resat challenge 연동

--- data/home_list.js
+++ data/home_list.js
@@ -1,11 +1,19 @@
 export const home_list_data = [
   {
-    id: 1,
-    title: "Nomad Coders",
-    date: "2021.06",
-    desc: "온라인 코딩 강의 [노마드 코더] - JS 크롬 앱 만들기 강의의 결과물입니다.",
-    mainImg: "images/home/nomad_chrome.png",
-    path: "pages/nomadChrome/index.html",
+    id: 4,
+    title: "Baseball Game",
+    date: "2023.08",
+    desc: "랜덤으로 정해진 숫자를 야구 게임 룰에 적용해 맞추는 게임입니다.",
+    mainImg: "images/home/baseBall.jpg",
+    path: "pages/baseBall/index.html",
+  },
+  {
+    id: 3,
+    title: "RESAT_Farming_Challenge",
+    date: "2023.06",
+    desc: "미니인턴에서 진행한 프론트엔드 10일 챌린지 결과물입니다.",
+    mainImg: "images/home/resat.png",
+    path: "pages/resatChallenge/index.html",
   },
   {
     id: 2,
@@ -16,11 +24,11 @@ export const home_list_data = [
     path: "pages/westagram/Login/index.html",
   },
   {
-    id: 3,
-    title: "Baseball Game",
-    date: "2023.08",
-    desc: "랜덤으로 정해진 숫자를 야구 게임 룰에 적용해 맞추는 게임입니다.",
-    mainImg: "images/home/baseBall.jpg",
-    path: "pages/baseBall/index.html",
+    id: 1,
+    title: "Nomad Coders",
+    date: "2021.06",
+    desc: "온라인 코딩 강의 [노마드 코더] - JS 크롬 앱 만들기 강의의 결과물입니다.",
+    mainImg: "images/home/nomad_chrome.png",
+    path: "pages/nomadChrome/index.html",
   },
 ];
--- utils/addDefaultPath.js
+++ utils/addDefaultPath.js
@@ -1,7 +1,5 @@
 export function addDefaultPath(url) {
   const path = window.location.pathname;
 
-  path.includes("VanillaJS_Practice")
-    ? (location.href = `/VanillaJS_Practice${url}`)
-    : (location.href = url);
+  path.includes("VanillaJS_Practice") ? (location.href = `/VanillaJS_Practice/${url}`) : (location.href = url);
 }
