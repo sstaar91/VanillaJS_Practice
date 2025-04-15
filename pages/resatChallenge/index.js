7c808c379452ddc626dd251be4435307d2ab5f6c
sstaar91 <sstaar91@gmail.com>
2025-04-15 10:52:35 +0900

    chore : path 에러 처리

--- pages/resatChallenge/index.js
+++ pages/resatChallenge/index.js
@@ -1,3 +1,5 @@
+import { addDefaultPath } from "../../utils/addDefaultPath.js";
+
 const logo = document.getElementsByClassName("logo")[0];
 logo.addEventListener("click", () => {
   addDefaultPath("/");
